e9bb8cb2f13784550026412a66427258569ba718
Natanniel <natanniel.alves@outlook.com>
2021-09-29 16:59:24 -0300

    deploy

--- src/pages/guardiao/cadastro/dados.js
+++ src/pages/guardiao/cadastro/dados.js
@@ -1,20 +1,54 @@
 import React, { useState } from 'react';
 import { Container } from '../../../components/globals'
-import { Titulo, Step, Titulo2, Row, Divider, AddContact, BtnSalvar ,BtnVoltar} from './styled'
+import { Titulo, Step, Titulo2, Row, Divider, AddContact, BtnSalvar, BtnVoltar } from './styled'
 import CpfCnpj from "@react-br-forms/cpf-cnpj-mask";
 import InputMask from "react-input-mask";
-
+import toastr from "toastr";
+import moment from "moment";
 // import { Container } from './styles';
 
-function Cadastro({aprovado}) {
+function Cadastro({ aprovado }) {
+
+
+    const [nome, setNome] = useState("");
+    const [sobrenome, setSobrenome] = useState("");
+    const [email, setEmail] = useState("");
+    const [data, setData] = useState("");
+    const [genero, setGenero] = useState("0");
+
+    const [endereco, setEndereco] = useState("");
+    const [numero, setNumero] = useState("");
+    const [complemento, setComplemento] = useState("");
 
     const [cpfCnpj, setCpfCnpj] = useState("");
     const [mask, setMask] = useState("");
     const [contatos, setContatos] = useState([]);
 
     let submit = () => {
-        aprovado();
-        window.scrollTo(0, 0)
+        alert();
+        const testEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (testEmail.test(String(email).toLowerCase()))
+            if (nome.length > 2)
+                if (sobrenome.length > 2)
+                    if (moment(data, 'DD/MM/YYYY', true).isValid())
+                        if (genero != 'selecione' && genero != '')
+
+                        else
+                            toastr.success("Você precisa informar o seu gênero");
+                    else
+                        toastr.error("Data de nascimento inválido !");
+                else
+                    toastr.error("Sobrenome inválido !");
+            else
+                toastr.error("Nome inválido !");
+        else
+            toastr.error("Email inválido !");
+
+
+
+
+
+
     }
 
 
@@ -28,30 +62,30 @@ function Cadastro({aprovado}) {
                 <Row>
                     <fieldset>
                         <legend>Nome</legend>
-                        <input placeholder="Seu nome" />
+                        <input placeholder="Seu nome" value={nome} onChange={(e) => setNome(e.target.value)} />
                     </fieldset>
                     <fieldset>
                         <legend>Sobrenome</legend>
-                        <input placeholder="Sobrenome" />
+                        <input placeholder="Sobrenome" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} />
                     </fieldset>
                 </Row>
                 <Row>
                     <fieldset>
                         <legend>E-mail</legend>
-                        <input placeholder="Seu melhor e-mail" />
+                        <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Seu melhor e-mail" />
                     </fieldset>
                 </Row>
                 <Row>
                     <fieldset>
                         <legend>Data de Nascimento</legend>
-                        <input type="date" />
+                        <input type="date" value={data} onChange={(e) => setData(e.target.value)} />
                     </fieldset>
                     <fieldset>
                         <legend>Gênero</legend>
-                        <select>
+                        <select value={genero} onChange={(e) => setGenero(e.target.value)}>
                             <option value="selecione">Selecione ...</option>
-                            <option value="selecione">Feminino</option>
-                            <option value="selecione">Masculino</option>
+                            <option value="feminino">Feminino</option>
+                            <option value="masculino">Masculino</option>
                         </select>
                     </fieldset>
                 </Row>
@@ -65,17 +99,17 @@ function Cadastro({aprovado}) {
                 <Row>
                     <fieldset>
                         <legend>Seu endereço</legend>
-                        <input placeholder="Seu endereço" />
+                        <input placeholder="Seu endereço" value={endereco} onChange={(e) => setEndereco(e.target.value)} />
                     </fieldset>
                 </Row>
                 <Row>
                     <fieldset style={{ width: '135px', flex: 'none' }}>
                         <legend>Numero</legend>
-                        <input placeholder="Numero" />
+                        <input placeholder="Numero" value={numero} onChange={(e) => setNumero(e.target.value)} />
                     </fieldset>
                     <fieldset style={{ width: '338px', flex: 'none' }}>
                         <legend>Complemento</legend>
-                        <input placeholder="Apartamento, bloco, conjunto, referência" />
+                        <input placeholder="Apartamento, bloco, conjunto, referência" value={complemento} onChange={(e) => setComplemento(e.target.value)} />
                     </fieldset>
                 </Row>
                 <Row>
@@ -131,7 +165,7 @@ function Cadastro({aprovado}) {
 
             <Divider />
             <section>
-                <Row style={{flexDirection:'row-reverse'}}>                  
+                <Row style={{ flexDirection: 'row-reverse' }}>
                     <BtnSalvar onClick={() => submit()}>Salvar e continuar</BtnSalvar>
                     <BtnVoltar>Voltar</BtnVoltar>
                 </Row>
